2325a9c8f0cb045ba3751c9c1519f6a35c9651ec
ldh9282 <ldh9282@naver.com>
2023-09-27 17:11:24 +0900

    sec01

---
+++ sec01_installation/basics.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var num1 = 345;
+var str1 = "abcd";
+console.log("hello");
+console.log("hello1");
+console.log("hello2");
---
+++ sec01_installation/basics.ts
@@ -0,0 +1,10 @@
+export {};
+
+let num1: number = 345;
+const str1: string = "abcd";
+
+console.log("hello");
+
+console.log("hello1");
+
+console.log("hello2");
